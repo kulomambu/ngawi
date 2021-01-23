3c303d1d6b3623538b5ca6ff90e0c3cf285a11be
kulomambu <77854129+kulomambu@users.noreply.github.com>
2021-01-23 12:39:17 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+
+// input key
+$key = "AIzaSyCL0VbkYb31UB-SgGT14DnF1E0EbqpVd10";
+
+// input refresh token
+$refresh_token = "AOvuKvRQNNXpIHLT3WnLG1ilYOiJ_CmxDNftFyV5o_Gq9sLXOI1uuvm_g6uznAx4IgYcKSdSB4eeVDxXQvU7091AHMk8sm7gWslfhFVguEkI17mmG1p_5sHZ5hJEllzd9YnuvpzfA2-S_RnmZ1uR8_O3d2WC3g5Aukm0YQM2Wq85JHA8fD_FHQ91JOc0quxjh9NITNogH49SL3pgCaLV29Io9AP63PQpH5hVrmDOVMo6mh35HQw2SOzm5Ft9_yvzGaZ2MKMJfx1Zbfw5-K6KHGl1ENZRTlVapJvZ947TBtQ9Y4GON0I20AwwIvCWIJx_m8YaPm6nogJ3jBtOqZTDVf7J8S9LN59kSAKE_9xmJ91lc_V9b1lEH8r62tpr528fpDArBo3gSmRlwil_6tMyDLSHLCCQ-WG7sPBxvXiG2ka1174poDdCCHvliH0pYH-QkVO5ajQm-tL12C4Yo8H6dv4upRt2VtqFeg";
+
+// input user
+$user_id = "t6NkM1kAwsXl9Rl7bilMaau7qOi1";
+
+
+  // jika poin tidak bertambah itu limit ngab //
